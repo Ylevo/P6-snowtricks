2af00be6f9c65943fcb39edd2735e153a93bbccd
Ylevo <4r2qobaf@anonaddy.me>
2023-04-28 07:10:48 +0200

    feat: added trick edition

--- src/Controller/EditTrickController.php
+++ src/Controller/EditTrickController.php
@@ -9,6 +9,7 @@ use App\Repository\MediaTypeRepository;
 use App\Service\ImageUploader;
 use App\Service\Thumbnailer;
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,25 +19,28 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class CreateTrickController extends AbstractController
+class EditTrickController extends AbstractController
 {
-    #[Route(path: '/new-trick', name: 'app_trick_create')]
-    public function newTrick(Request $request,
+    #[Route(path: '/edit-trick/{slug}', name: 'app_trick_edit')]
+    public function editTrick(Trick $trick,
+                             Request $request,
                              EntityManagerInterface $entityManager,
                              MediaRepository $mediaRepository,
                              MediaTypeRepository $mediaTypeRepository,
                              ImageUploader $imageUploader): Response
     {
-        $trick = new Trick();
-        $trick->setMediaCover($mediaRepository->findOneBy(['url' => 'default_cover.jpeg']));
-
         $form = $this->createForm(TrickFormType::class, $trick);
+        try{
+            dump($trick->getImageMedias());
+        }
+        catch (Exception $e)
+        {
+            return dump($trick);
+        }
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-            $trick->setUser($this->getUser());
 
             $imageMediasListFromForm = $form->get('imageMedias');
             $imageMedias = $trick->getImageMedias();
@@ -56,10 +60,10 @@ class CreateTrickController extends AbstractController
                 $trick->addMedia($videoMedia);
             }
 
+            $this->addFlash('success', 'Modifications to "'.$trick->getName().'" successfully saved.');
+
             $entityManager->persist($trick);
             $entityManager->flush();
-
-            $this->addFlash('success', 'New trick "'.$trick->getName().'" successfully added.');
 
             return $this->redirectToRoute('app_trick_show', ['slug' => $trick->getSlug()]);
         }
